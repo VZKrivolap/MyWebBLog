cc141d9b6f7251dcaaba49adea502203debccc43
VZKrivolap <vz984762@gmail.com>
2024-01-18 03:34:45 +0300

    Initial commit.

---
+++ src/main/java/org/example/mywebblog/MyWebBLogApplication.java
@@ -0,0 +1,13 @@
+package org.example.mywebblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWebBLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWebBLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/mywebblog/MyWebBLogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mywebblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWebBLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
